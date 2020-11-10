43150337f475809efd1544d0dd36aeb98778188d
Alex Goodwin <hagoodwi@gmail.com>
2020-11-10 13:37:11 -0800

    test progress

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -3,6 +3,6 @@ import Plant from './../src/plant.js';
 describe('Plant', () => {
 
   test('should correctly create a plant object', () => {
-    expect(changeState("soil")).toEqual(true);
+    expect(changeState(feed)).toEqual(true);
   });
 });
