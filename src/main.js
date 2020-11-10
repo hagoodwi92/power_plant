909e1579d9d25900f16419177676882bc4902672
Alex Goodwin <hagoodwi@gmail.com>
2020-11-10 11:34:27 -0800

    add ui to main.js

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import './css/styles.css';
 import Plant from './plant.js';
 
 $(document).ready(function() {
-  $('#triangle-checker-form').submit(function(event) {
-    event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3);
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
+
+  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+  
+    $('#feed').click(function() {
+      const newState = stateControl(blueFood);
+      $('#soil-value').text(`Soil: ${newState.soil}`);
+    });
+  
+  // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  
+    $('#show-state').click(function() {
+      // We just need to call stateControl() without arguments to see our current state.
+      const currentState = stateControl();
+      $('#soil-value').text(`Soil: ${currentState.soil}`);
+    });
   });
-});
